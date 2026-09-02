6bdf10bfe762a9b6d0c03e89302dd05de662015d
Dnv14 <diego.navarro.140706@gmail.com>
2026-09-01 21:54:49 -0700

    html y si sirve con postman
    terminado

--- src/main/java/com/example/MiPrimerServidorWeb/HelloController.java
+++ src/main/java/com/example/MiPrimerServidorWeb/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.MiPrimerServidorWeb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // registramos clase como el controlador http
 @RequestMapping("/api") //prefijo de las rutas
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(){return "Hola desde Springbot";}
 
-    @GetMapping("/saludo")
-    public String saludo(@RequestParam String nombre){
-        return "hola " + nombre ;
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hola desde Springbot";
     }
 
+    @GetMapping("/saludo")
+    public String saludo(@RequestParam String nombre) {
+        return "hola " + nombre;
+    }
+
+    @PostMapping("/mensaje")
+    public String mensaje(@RequestBody String texto) {
+        return "Texto recibido: " + texto;
+    }
 }
